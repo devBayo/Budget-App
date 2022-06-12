3250c36340b00f4f8c4342b3373906d9dbaa21f3
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-06-12 22:22:31 +0100

    selected html elements

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Targetting Elements
+const labelMonth = document.querySelector('.month');
+const labelBalance = document.querySelector('.balance');
+const labelTotalIncome = document.querySelector('.total-income');
+const labelTotalExpenses = document.querySelector('.total-expenses');
+const sign = document.querySelector('.sign');
+const description = document.querySelector('.desc');
+const amount = document.querySelector('.amount');
