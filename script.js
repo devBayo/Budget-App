e39ab03fd792c2f889803beb27273a8741e9373e
devBayo <71531531+devBayo@users.noreply.github.com>
2022-08-01 01:08:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Targetting Elements
-// //
+//
 const labelMonth = document.querySelector('.month');
 const labelBalance = document.querySelector('.balance');
 const labelTotalIncome = document.querySelector('.total-income');
